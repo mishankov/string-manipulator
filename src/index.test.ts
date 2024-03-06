834b4b7123bdf79d69588ce9982884ad627c66c9
Denis Mishankov <mishankov@mail.com>
2024-03-06 23:25:55 +0300

    prettier

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,8 +1,7 @@
-import { expect, it } from "vitest";
-import { doManipulation, type TManipulation } from "$lib/manipulations";
+import { expect, it } from 'vitest';
+import { doManipulation, type TManipulation } from '$lib/manipulations';
 
-
-it("Test replace manipulation", () => {
-  expect(doManipulation("\\3", {type: "replace", from: "\\\\3", to: "result"})).eq("result")
-  expect(doManipulation("\\", {type: "replace", from: "\\\\", to: "result"})).eq("result")
-})
+it('Test replace manipulation', () => {
+	expect(doManipulation('\\3', { type: 'replace', from: '\\\\3', to: 'result' })).eq('result');
+	expect(doManipulation('\\', { type: 'replace', from: '\\\\', to: 'result' })).eq('result');
+});
