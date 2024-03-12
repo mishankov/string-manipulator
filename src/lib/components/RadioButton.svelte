b5976d4db7d40ce828243eea9a79532b272fcae9
Denis Mishankov <mishankov@mail.com>
2024-03-12 21:50:12 +0300

    format

--- src/lib/components/RadioButton.svelte
+++ src/lib/components/RadioButton.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
-  interface Option {
-    value: string;
-    label: string;
-  }
+	interface Option {
+		value: string;
+		label: string;
+	}
 
-  export let options: Option[];
-  export let selected: string;
+	export let options: Option[];
+	export let selected: string;
 </script>
 
 {#each options as option}
-  <label>
-    <input type="radio" bind:group={selected} value={option.value} />
-    {option.label}
-  </label>
+	<label>
+		<input type="radio" bind:group={selected} value={option.value} />
+		{option.label}
+	</label>
 {/each}
--- src/lib/manipulations/components/Trim.svelte
+++ src/lib/manipulations/components/Trim.svelte
@@ -1,35 +1,35 @@
 <script lang="ts" context="module">
-  import type { ManipulationBase } from '..';
-  import RadioButton from '$lib/components/RadioButton.svelte';
+	import type { ManipulationBase } from '..';
+	import RadioButton from '$lib/components/RadioButton.svelte';
 
-  export interface TrimManipulation extends ManipulationBase {
-    type: 'trim';
-    trimType: string;
-  }
+	export interface TrimManipulation extends ManipulationBase {
+		type: 'trim';
+		trimType: string;
+	}
 
-  const options = [
-    {
-      value: 'leading',
-      label: 'Leading'
-    },
-    {
-      value: 'trailing',
-      label: 'Trailing'
-    },
-    {
-      value: 'all',
-      label: 'All'
-    }
-  ]
+	const options = [
+		{
+			value: 'leading',
+			label: 'Leading'
+		},
+		{
+			value: 'trailing',
+			label: 'Trailing'
+		},
+		{
+			value: 'all',
+			label: 'All'
+		}
+	];
 </script>
 
 <script lang="ts">
-  export let { trimType }: TrimManipulation = { trimType: 'all', type: 'trim' };
+	export let { trimType }: TrimManipulation = { trimType: 'all', type: 'trim' };
 </script>
 
 <div>
-  <span>Trim </span>
-  <RadioButton bind:selected={trimType} options={options} />
+	<span>Trim </span>
+	<RadioButton bind:selected={trimType} {options} />
 </div>
 
 <style>
