a7cd147677b35bb8be606baf608b7ca319d81fb4
Denis Mishankov <mishankov@mail.com>
2024-03-12 21:53:10 +0300

    add test

--- src/index.test.ts
+++ src/index.test.ts
@@ -5,3 +5,9 @@ it('Test replace manipulation', () => {
 	expect(doManipulation('\\3', { type: 'replace', from: '\\\\3', to: 'result' })).eq('result');
 	expect(doManipulation('\\', { type: 'replace', from: '\\\\', to: 'result' })).eq('result');
 });
+
+it('Test trim manipulation', () => {
+	expect(doManipulation('   input    ', { type: 'trim', trimType: 'leading' })).eq('input    ');
+	expect(doManipulation('   input    ', { type: 'trim', trimType: 'trailing' })).eq('   input');
+	expect(doManipulation('   input    ', { type: 'trim', trimType: 'all' })).eq('input');
+});
